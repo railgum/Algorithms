248bdeac1607dbf3aeb84435be946e6c1bb52d03
Rail <railbolo@gmail.com>
2023-06-18 13:04:02 +0400

    new

--- dir_2/src/Sem_2/QuickSort.java
+++
@@ -1,9 +0,0 @@
-package Sem_2;
-
-public class QuickSort {
-    public static void quickSort(int[] array, int leftBorder, int rightBorder){
-        int pivot = leftBorder + (leftBorder+rightBorder)/2;
-        int leftMarker = leftBorder;
-        int rightMarker = rightBorder;
-    }
-}
