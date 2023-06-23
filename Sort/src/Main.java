de43ee05dd0fda5a392644085214ac78c7b573e3
Rail <railbolo@gmail.com>
2023-06-23 20:04:59 +0400

    start lesson3

--- Sort/src/Main.java
+++ Sort/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         int[] array = randomFill(6);
         print(array);
 //        BubbleSort.bubleSort(array);
-        HeapSort.sort(array);
+//        HeapSort.sort(array);
         print(array);
     }
 
