a57003b4bf52138989551206d6c4fcacb9f4d388
Rail <railbolo@gmail.com>
2023-06-25 20:17:58 +0400

    add redBlackTree

--- Sort/src/Main.java
+++ Sort/src/Main.java
@@ -1,7 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Random;
 import Lesson_2.*;
 import HomeWork_2.*;
 import Sem_3.*;
+import Lesson_4.*;
 public class Main {
     public static void main(String[] args) {
 //        int[] array = randomFill(6);
@@ -9,15 +13,28 @@ public class Main {
 //        BubbleSort.bubbleSort(array);
 //        HeapSort.sort(array);
 //        print(array);
-        LinkedListSem list = new LinkedListSem();
+     /*   LinkedListSem list = new LinkedListSem();
         list.add(5);
         list.add(2);
         list.add(7);
         list.add(1);
         list.print();
         list.removeAt(2);
-        list.print();
+        list.print();*/
+        final RedBlackTree tree = new RedBlackTree();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
+           while (true){
+               try {
+                   int value = Integer.parseInt(reader.readLine());
+                   tree.add(value);
+                   System.out.println("finish");
+               } catch (Exception ignored){
 
+               }
+           }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static int[] randomFill(int size) {
